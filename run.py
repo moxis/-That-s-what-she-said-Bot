a247c9d807598266b4828f68cdbaa15474bc76dd
Nguyen Nguyen <mail@koxi.pw>
2017-10-17 14:27:01 +0200

    good bot

--- run.py
+++ run.py
@@ -13,21 +13,25 @@ def did_she_say(message):
     text = snowball_stemming(remove_punctuation(message.text.lower()))
     response = cl.prob_classify(text)
     LOGGER.info('"{}": {} {}'.format(message.text, round(response.prob('pos'), 2), round(response.prob('neg'), 2)))
-    if response.prob('pos') > 0.94 and len(message.text.split()) > 2:
-        bot.send_message(message.chat.id, "That's what she said.")
+    if response.prob('pos') > 0.92 and len(message.text.split()) > 2:
+        bot.send_message(message.chat.id, "That's what she said!")
 
 @bot.message_handler(func=lambda message: message.reply_to_message is not None and 'stat' in message.text.lower())
 def return_statistics(message):
     text = snowball_stemming(remove_punctuation(message.reply_to_message.text.lower()))
     response = cl.prob_classify(text)
-    bot.send_message(message.chat.id, 'I am *{}%* certain that this was my time to shine!'.format(round(response.prob('pos'), 2) * 100), parse_mode='Markdown')
+    bot.send_message(message.chat.id, 'I am *{}%* certain that she said this!'.format(round(response.prob('pos')*100, 2)), parse_mode='Markdown')
 
-@bot.message_handler(func=lambda message: message.reply_to_message is not None and 'bad bot' in message.text.lower())
-def bad_bot(message):
+@bot.message_handler(func=lambda message: message.reply_to_message is not None and message.text.lower() in ['bad bot', 'good bot'])
+def retrain(message):
+    if message.text == 'bad bot':
+        mode = 'neg'
+    else:
+        mode = 'pos'
     text = snowball_stemming(remove_punctuation(message.reply_to_message.text.lower()))
-    cl.update([(text, 'neg') for x in range(0, 11)])
+    cl.update([(text, mode) for x in range(0, 10)])
     with open('network.pickle', 'wb') as handler:
         pickle.dump(cl, handler)
     bot.send_message(message.chat.id, '`RETRAINED! ALWAYS LEARNING FROM MY MISTAKES`', parse_mode='Markdown')
-    
+
 bot.polling(none_stop=True)
